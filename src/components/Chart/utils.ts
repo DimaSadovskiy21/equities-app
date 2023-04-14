896935e5c3313f52e17a53ba6a57519abdde983f
DimaSadovskiy21 <dimasadovsky21@gmail.com>
2023-04-14 11:24:23 +0300

    fix commit

--- src/components/Chart/utils.ts
+++ src/components/Chart/utils.ts
@@ -18,12 +18,13 @@ export const getChartCurrentEquitie = (
   lastSalePrice: number,
   lastUpdated: number,
   steps: number = 10,
-  stepSize: number = 0.001,
+  stepSize: number = 0.1,
   timeStep: number = 600000
 ) => {
   const data = [];
 
   let startTime = lastUpdated;
+  
   for (let i = 0; i < steps; i++) {
     let price =
       lastSalePrice + (Math.random() < 0.5 ? -1 : 1) * Math.random() * stepSize;
